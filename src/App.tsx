cd14fd7e360269ece7782a34d1479abcdc54e1b1
RBM Web <lais.martins@rbmweb.com.br>
2023-06-29 15:12:48 -0300

    App component edited

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from 'react';
+import { useOnClickOutside } from './hooks';
+
+import Burger from './components/Burger/Burger';
+import Menu from './components/Menu/Menu';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(false);
+  const node = useRef<HTMLDivElement>(null);
+  useOnClickOutside(node, () => setOpen(false));
 
   return (
     <>
+      <div ref={node}>
+        <Burger open={open} setOpen={setOpen} />
+        <Menu open={open} />
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hello. This is burger menu tutorial</h1>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1858/1858002.png"
+          alt="burger icon"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
