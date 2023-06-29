6102c6b2bf8813c086ef3bc02ac16474367b609a
RBM Web <lais.martins@rbmweb.com.br>
2023-06-29 15:09:33 -0300

    ThemeProvider and GlobalStyles configured

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+import { ThemeProvider } from 'styled-components';
+import { theme } from './Theme.ts';
+import { GlobalStyles } from './GlobalStyles.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
