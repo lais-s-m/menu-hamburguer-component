ee2399b98512a071703482bed2c10d9f1c283bed
RBM Web <lais.martins@rbmweb.com.br>
2023-06-29 15:13:17 -0300

    Hook open/close hamburguer menu created

---
+++ src/hooks.ts
@@ -0,0 +1,21 @@
+import { RefObject, useEffect } from 'react';
+
+export const useOnClickOutside = (
+  ref: RefObject<HTMLDivElement>,
+  handler: (event: any) => void
+) => {
+  useEffect(() => {
+    const listener = (event: any) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      } else {
+        handler(event);
+      }
+    };
+    document.addEventListener('mousedown', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+    };
+  }, [ref, handler]);
+};
