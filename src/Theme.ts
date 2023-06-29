a79574ea381e1bb64ef0a5a6fc50cc81b29f7a44
RBM Web <lais.martins@rbmweb.com.br>
2023-06-29 15:10:32 -0300

    Theme file created

---
+++ src/Theme.ts
@@ -0,0 +1,13 @@
+interface ITheme {
+  primaryDark: string;
+  primaryLight: string;
+  primaryHover: string;
+  mobile: string;
+}
+
+export const theme: ITheme = {
+  primaryDark: '#0D0C1D',
+  primaryLight: '#EFFFFA',
+  primaryHover: '#343078',
+  mobile: '576px',
+};
